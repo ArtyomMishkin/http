e20343af9afd0a3dfc5efaa31d414732695f4aaa
ArtyomMishkin <95028786+ArtyomMishkin@users.noreply.github.com>
2025-10-05 21:57:41 +0300

    Update make.ps1

--- make.ps1
+++ make.ps1
@@ -3,7 +3,7 @@ param([string]$Target)
 switch ($Target) {
     "run"   { go run ./cmd/server }
     "build" { go build -o bin/server.exe ./cmd/server }
-    "test"  { go test ./internal/api/... }
+    "test"  { go test -v ./internal/api/... }
     default { 
         Write-Host "Usage: .\make.ps1 [run|build|test]"
         Write-Host "  run   - go run ./cmd/server"
